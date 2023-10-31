c9b33c0c8ea9cb2aef633e4891b9d9da929c3c48
matheusburey <matheushburei.99@gmail.com>
2023-10-31 19:51:43 -0300

    feat(mobile): add lib and configure expo-google-fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-white-500 flex-1 items-center justify-center">
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className="font-alt text-gray-50 font-bold text-4xl">
+        I love you
+      </Text>
       <StatusBar style="auto" translucent />
     </View>
   )
