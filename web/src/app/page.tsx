ec2f2e3e86f93c6689c7f8bf0c5710e9709ceebe
matheusburey <matheushburei.99@gmail.com>
2024-06-13 16:11:07 -0300

    validation

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,23 @@
 import EmptyMemories from '@/components/EmptyMemories'
+import { api } from '@/lib/api'
+import { cookies } from 'next/headers'
 
-export default function Home() {
-  return <EmptyMemories />
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get('token')?.value
+  const response = await api.get('/memories', {
+    headers: { Authorization: `Bearer ${token}` },
+  })
+  const memories = response.data
+
+  if (!memories.length) {
+    return <EmptyMemories />
+  }
+
+  return <div>teste</div>
 }
